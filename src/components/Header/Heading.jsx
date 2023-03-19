6039ad4d60e01b6310cb3d7513f96034560b3f62
Jay_Anonymous <gnjakai@gmail.com>
2023-03-19 18:34:04 +0300

    :pullrebase

--- src/components/Header/Heading.jsx
+++ src/components/Header/Heading.jsx
@@ -1,10 +1,10 @@
 import "./Heading.css";
-import mySvg from "../../images/hbo-max.svg";
+
 
 const Heading = () => {
   return (
     <div className="MainHeading">
-      <img src={mySvg} alt="" className="logo2" />{" "}
+      
     </div>
   );
 };
