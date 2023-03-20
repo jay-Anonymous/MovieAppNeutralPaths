c031fab61c359ee6c32979177343f0f3077de9ef
Jay_Anonymous <gnjakai@gmail.com>
2023-03-20 11:36:21 +0300

    :env api key changes

--- src/components/MainNavbar/MainNav.jsx
+++ src/components/MainNavbar/MainNav.jsx
@@ -40,7 +40,7 @@ const MainNav = () => {
               </Link>
             </li>
             <li className="nav-item  nav__link">
-              <Link className="nav-link" to="/treading">
+              <Link className="nav-link" to="/trending">
                 <WhatshotIcon
                   style={{
                     fontSize: "17px",
@@ -49,7 +49,7 @@ const MainNav = () => {
                     marginRight: "2px",
                   }}
                 />
-                Treading
+                
               </Link>
             </li>
             <li className="nav-item  nav__link">
